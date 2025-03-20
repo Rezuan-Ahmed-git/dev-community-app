7e553c0f578e0f8921d687708729a49718e90968
Rezuan-Ahmed-git <rezuanahmed.dev@gmail.com>
2025-03-20 19:40:55 +0600

    Theme setup, Font setup, Assets added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => (
-  <h1 className="h1-bold text-primary-500">Welcome to the world of Next.js</h1>
+  <>
+    <h1 className="h1-bold text-primary-500">
+      Welcome to the world of Next.js
+    </h1>
+    <h1 className="h1-bold font-space-grotesk text-primary-500">
+      Welcome to the world of Next.js
+    </h1>
+  </>
 );
 
 export default Home;
