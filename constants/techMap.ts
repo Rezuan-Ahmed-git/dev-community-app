96c04cd86959b2d1dd5b217276b148ad62f08dda
Rezuan-Ahmed-git <rezuanahmed.dev@gmail.com>
2025-03-23 04:11:08 +0600

    Implement Right Sidebar

---
+++ constants/techMap.ts
@@ -0,0 +1,8 @@
+export const techMap: { [key: string]: string } = {
+  javascript: "devicon-javascript-plain",
+  js: "devicon-javascript-plain colored",
+  typescript: "devicon-typescript-plain",
+  ts: "devicon-typescript-plain colored",
+  nextjs: "devicon-nextjs-plain",
+  react: "devicon-react-plain colored",
+};
