d442c446bce763cd125316d598047e64c675f23e
Rezuan-Ahmed-git <rezuanahmed.dev@gmail.com>
2025-03-24 07:00:37 +0600

    Implement Updating URL via Local Search bar from the Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,37 @@
-import { auth } from "@/auth";
+import Link from "next/link";
+
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
   return (
     <>
-      <h1 className="h1-bold text-primary-500">
-        Welcome to the world of Next.js
-      </h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section>
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };
