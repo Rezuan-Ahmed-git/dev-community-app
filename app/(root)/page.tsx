411276f52fac77784b94c1dcdce849ecff60162a
Rezuan-Ahmed-git <rezuanahmed.dev@gmail.com>
2025-03-24 14:54:46 +0600

    Implement Question Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
@@ -17,11 +18,12 @@ const questions = [
     author: {
       _id: "1",
       name: "John Doe",
+      image: "https://cdn-icons-png.flaticon.com/512/6858/6858504.png",
     },
     upvotes: 10,
     answers: 5,
     views: 100,
-    createdAt: new Date(),
+    createdAt: new Date("2025-01-23"),
   },
   {
     _id: "2",
@@ -34,11 +36,12 @@ const questions = [
     author: {
       _id: "1",
       name: "John Doe",
+      image: "https://cdn-icons-png.flaticon.com/512/6858/6858504.png",
     },
     upvotes: 10,
     answers: 5,
     views: 100,
-    createdAt: new Date(),
+    createdAt: new Date("2025-03-23"),
   },
 ];
 
@@ -86,7 +89,7 @@ const Home = async ({ searchParams }: SearchParams) => {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
-          <h1 key={question._id}> {question.title} </h1>
+          <QuestionCard key={question._id} question={question} />
         ))}
       </div>
     </>
--- constants/routes.ts
+++ constants/routes.ts
@@ -3,7 +3,8 @@ const ROUTES = {
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   ASK_QUESTION: "/ask-question",
-  PROFILE: (id: string) => `profile/${id}`,
+  PROFILE: (id: string) => `/profile/${id}`,
+  QUESTION: (id: string) => `/question/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
 };
 
